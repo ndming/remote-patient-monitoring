c0a25ff01cb76cd0c0aeebd0275c292745117427
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-03-26 12:14:06 +0700

    add `isPasswordValid` function

--- Urgent/app/src/main/java/com/hescul/urgent/core/utils/Utility.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/utils/Utility.kt
@@ -2,12 +2,21 @@ package com.hescul.urgent.core.utils
 
 class InfoValidator {
     companion object {
-        private const val EMAIL_REGEX_PATTERN = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        private const val EMAIL_REGEX_PATTERN = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[{3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
         private const val PASSWORD_MIN_LENGTH = 8
 
         @JvmStatic
         fun isEmailValid(email: String): Boolean {
             return EMAIL_REGEX_PATTERN.toRegex().matches(email)
+        }
+
+        @JvmStatic
+        fun isPasswordValid(password: String): Boolean {
+            return pwMeetsLengthReq(password)
+                    && pwMeetsSpecialCharReq(password)
+                    && pwMeetsNumberReq(password)
+                    && pwMeetsUpperCaseReq(password)
+                    && pwMeetsLowerCaseReq(password)
         }
 
         @JvmStatic
