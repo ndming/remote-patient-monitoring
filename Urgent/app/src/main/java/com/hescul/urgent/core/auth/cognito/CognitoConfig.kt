7dff55e8ddebed55e477b64effac424631aa1e7e
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-03-29 20:30:04 +0700

    rename constant members

--- Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
@@ -11,9 +11,9 @@ object CognitoConfig  {
     const val CLIENT_SECRET = "11ih4or60jidu22m9a89oil75r0rekf1a74rf4ocaai9nlhqvj90"
     val REGION = Regions.US_EAST_1
 
-    const val DEFAULT_CONNECTION_TIMEOUT = 15 * 1000
-    const val DEFAULT_SOCKET_TIMEOUT = 15 * 1000
-    const val DEFAULT_MAX_CONNECTIONS = 10
-    val DEFAULT_USER_AGENT: String = VersionInfoUtils.getUserAgent()
+    const val CONNECTION_TIMEOUT = 15 * 1000
+    const val SOCKET_TIMEOUT = 15 * 1000
+    const val MAX_CONNECTIONS = 10
+    val USER_AGENT: String = VersionInfoUtils.getUserAgent()
 }
 
