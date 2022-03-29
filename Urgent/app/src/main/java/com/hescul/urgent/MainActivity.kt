ec8738a7ee234f2bec2b4b73f91c7514578b3dd6
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-03-29 20:34:26 +0700

    reorganize ViewModels and migrate to compose navigation

--- Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
@@ -7,31 +7,57 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.hescul.urgent.ui.screens.signup.SignUpScreen
+import androidx.navigation.compose.rememberNavController
+import com.hescul.urgent.model.UrgentViewModel
+import com.hescul.urgent.model.UrgentViewModelFactory
+import com.hescul.urgent.navigation.UrgentNavHost
+import com.hescul.urgent.navigation.UrgentScreen
+import com.hescul.urgent.ui.screens.confirm.ConfirmViewModel
 import com.hescul.urgent.ui.screens.signup.SignUpViewModel
 import com.hescul.urgent.ui.theme.UrgentTheme
 
 class MainActivity : ComponentActivity() {
-
-    private val signUpViewModel by viewModels<SignUpViewModel>()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val urgentViewModel = initViewModels()
         setContent {
-            UrgentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    SignUpScreen(
-                        signUpViewModel = signUpViewModel,
-                    )
-                }
-            }
+            UrgentApp(urgentViewModel)
         }
 
+    }
+
+    private fun initViewModels(): UrgentViewModel {
+        val signUpViewModel by viewModels<SignUpViewModel>()
+        val confirmViewModel by viewModels<ConfirmViewModel>()
+        val factory = UrgentViewModelFactory(
+            signUpViewModel = signUpViewModel,
+            confirmViewModel = confirmViewModel
+        )
+        val urgentViewModel by viewModels<UrgentViewModel>(
+            factoryProducer = { factory }
+        )
+        return urgentViewModel
+    }
+}
+
+@Composable
+fun UrgentApp(
+    urgentViewModel: UrgentViewModel,
+) {
+    UrgentTheme {
+        //val allScreens = UrgentScreen.values().toList()
+        val navController = rememberNavController()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            UrgentNavHost(
+                navController = navController,
+                startDestination = UrgentScreen.SignUp.name,
+                urgentViewModel = urgentViewModel,
+            )
+        }
     }
 }
