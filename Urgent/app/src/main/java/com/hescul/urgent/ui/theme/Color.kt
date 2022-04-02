b4991ec2f6f7a700f6e4c1094cd099043a144a2e
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-02 21:58:17 +0700

    add light theme colors

--- Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
@@ -3,9 +3,16 @@ package com.hescul.urgent.ui.theme
 import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
+// defaults
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// functional
 val Red700 = Color(0xFFD50000)
 val Green800 = Color(0xFF2E7D32)
+
+// light theme
+val BluePrimary = Color(0xFF1F5BD6)
+val BlueSecondary = Color(0xFFDCEEFD)
