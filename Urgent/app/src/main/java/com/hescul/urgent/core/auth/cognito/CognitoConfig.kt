95bb8d7477e996afc08dd08d6d945f6f26b62d45
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-02 21:58:02 +0700

    relocate debug tag attribute to `CognitoAuthenticator`

--- Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
@@ -4,8 +4,6 @@ import com.amazonaws.regions.Regions
 import com.amazonaws.util.VersionInfoUtils
 
 object CognitoConfig  {
-    const val DEBUG_TAG = "authCognito"
-
     const val POOL_ID = "us-east-1_JsBf49YSf"
     const val CLIENT_ID = "6beddanofjgqq8f8nb2o386ad6"
     const val CLIENT_SECRET = "11ih4or60jidu22m9a89oil75r0rekf1a74rf4ocaai9nlhqvj90"
