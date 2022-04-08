707191e9c0adc0b77aa9e5eadc3790f86e63d992
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:51:11 +0700

    add `OpeningViewModel` and `HomeViewModel`

--- Urgent/app/src/main/java/com/hescul/urgent/UrgentViewModels.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/UrgentViewModels.kt
@@ -1,11 +1,15 @@
 package com.hescul.urgent
 
 import com.hescul.urgent.ui.screens.confirm.ConfirmViewModel
+import com.hescul.urgent.ui.screens.home.HomeViewModel
 import com.hescul.urgent.ui.screens.login.LoginViewModel
+import com.hescul.urgent.ui.screens.opening.OpeningViewModel
 import com.hescul.urgent.ui.screens.signup.SignUpViewModel
 
 data class UrgentViewModels(
+    val openingViewModel: OpeningViewModel,
     val loginViewModel: LoginViewModel,
     val signUpViewModel: SignUpViewModel,
     val confirmViewModel: ConfirmViewModel,
+    val homeViewModel: HomeViewModel,
 )
