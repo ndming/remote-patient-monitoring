89d9768ae67a91c3c11952c451c2b7f4a6b0ff6d
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:58:25 +0700

    add raw string experiment

--- experiment/jsonSample.py
+++ experiment/jsonSample.py
@@ -8,5 +8,7 @@ PIPELINE_ATTRIBUTES = {
     "temper": 37.2,
 }
 
+MESSAGE_SJSON = "{\"cid\": \"RPMSOS0000\",\"size\": 3,\"time\": 1647923624.7255058,\"data\": {\"pulse\": {\"value\": 69.2,\"unit\": \"bpm\"},\"oxi\": {\"value\": 89.9,\"unit\": \"%\"},\"temper\": {\"value\": 38.0,\"unit\": \"\u00b0C\"}}}"
+
 with open('pipeline.json', 'w') as jfile:
     json.dump(PIPELINE_ATTRIBUTES, jfile)
