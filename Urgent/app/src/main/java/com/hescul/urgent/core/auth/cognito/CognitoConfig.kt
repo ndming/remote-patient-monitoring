aaf096a2239b55e16c194d64e0fb62931b4df30d
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:36:08 +0700

    add identity pool id and provider name for Cognito federated identity pool

--- Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/auth/cognito/CognitoConfig.kt
@@ -3,15 +3,16 @@ package com.hescul.urgent.core.auth.cognito
 import com.amazonaws.regions.Regions
 import com.amazonaws.util.VersionInfoUtils
 
+/**
+ * This object defines required information for AWS Cognito usage.
+ */
 object CognitoConfig  {
     const val POOL_ID = "us-east-1_JsBf49YSf"
     const val CLIENT_ID = "6beddanofjgqq8f8nb2o386ad6"
     const val CLIENT_SECRET = "11ih4or60jidu22m9a89oil75r0rekf1a74rf4ocaai9nlhqvj90"
     val REGION = Regions.US_EAST_1
 
-    const val CONNECTION_TIMEOUT = 15 * 1000
-    const val SOCKET_TIMEOUT = 15 * 1000
-    const val MAX_CONNECTIONS = 10
-    val USER_AGENT: String = VersionInfoUtils.getUserAgent()
+    const val IDENTITY_POOL_ID = "us-east-1:e1bf1dff-d174-49e2-91ab-4276e9a171c5"
+    const val PROVIDER_NAME = "cognito-idp.us-east-1.amazonaws.com/us-east-1_JsBf49YSf"
 }
 
