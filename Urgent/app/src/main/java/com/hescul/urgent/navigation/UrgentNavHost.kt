c62d2784432d162671e519621228dfab79fa907d
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:50:16 +0700

    migrate to singleton usage for the `CognitoAuthenticator` class

--- Urgent/app/src/main/java/com/hescul/urgent/navigation/UrgentNavHost.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/navigation/UrgentNavHost.kt
@@ -7,17 +7,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.hescul.urgent.R
 import com.hescul.urgent.UrgentViewModels
-import com.hescul.urgent.core.auth.cognito.CognitoAuthenticator
 import com.hescul.urgent.ui.screens.confirm.ConfirmScreen
 import com.hescul.urgent.ui.screens.home.HomeScreen
 import com.hescul.urgent.ui.screens.login.LoginScreen
+import com.hescul.urgent.ui.screens.opening.OpeningScreen
 import com.hescul.urgent.ui.screens.signup.SignUpScreen
 import com.hescul.urgent.ui.theme.SystemTheme
 import com.hescul.urgent.ui.theme.UrgentTheme
@@ -26,7 +23,6 @@ import com.hescul.urgent.ui.theme.UrgentTheme
 @Composable
 fun UrgentNavHost(
     startDestination: String,
-    cognito: CognitoAuthenticator,
     viewModels: UrgentViewModels,
     modifier: Modifier = Modifier
 ) {
@@ -42,57 +38,60 @@ fun UrgentNavHost(
                 navController = navController,
                 startDestination = startDestination,
             ) {
+                // Opening Screen
+                composable(
+                    route = UrgentScreen.Opening.name,
+                    enterTransition = { fadeIn(animationSpec = tween(400)) },
+                    exitTransition = { fadeOut(animationSpec = tween(200)) }
+                ) {
+                    val openingViewModel = viewModels.openingViewModel
+                    OpeningScreen(
+                        openingViewModel = openingViewModel,
+                        onDone = {
+                            navController.navigate(UrgentScreen.Login.name) {
+                                popUpTo(UrgentScreen.Opening.name) { inclusive = true }
+                            }
+                        }
+                    )
+                }
+
                 // Sign Up Screen
                 composable(
                     route = UrgentScreen.SignUp.name,
                     enterTransition = {
                         when (initialState.destination.route) {
                             UrgentScreen.Login.name -> slideIntoContainer(
-                                AnimatedContentScope.SlideDirection.Up,
-                                animationSpec = tween(700)
+                                towards = AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(durationMillis = NavConfig.ENTER_TRANSITION_DURATION)
                             )
-                            else -> EnterTransition.None
+                            else -> fadeIn(animationSpec = tween(durationMillis = NavConfig.ENTER_TRANSITION_DURATION))
                         }
                     },
                     exitTransition = {
-                        slideOutOfContainer(
-                            AnimatedContentScope.SlideDirection.Down,
-                            animationSpec = tween(500)
-                        )
+                        when (targetState.destination.route) {
+                            UrgentScreen.Login.name -> slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(durationMillis = NavConfig.EXIT_TRANSITION_DURATION)
+                            )
+                            else -> fadeOut(animationSpec = tween(durationMillis = NavConfig.EXIT_TRANSITION_DURATION))
+                        }
                     }
                 ) {
-                    val defaultFailCause = stringResource(id = R.string.ui_signUpScreen_defaultFailCause)
                     val signUpViewModel = viewModels.signUpViewModel
                     SignUpScreen(
                         signUpViewModel = signUpViewModel,
-                        onSignUpRequest = {
-                            cognito.signUpUser(
-                                name = signUpViewModel.nameTextInput,
-                                userId = signUpViewModel.emailTextInput,
-                                password = signUpViewModel.passwordTextInput,
-                                onSignUpSuccess = { user, signUpResult ->
-                                    signUpViewModel.onSignUpSuccess()
-                                    if (!signUpResult.isUserConfirmed) { // navigate to confirm screen
-                                        viewModels.confirmViewModel.reset()
-                                        viewModels.confirmViewModel.updateConfirmationIdentity(
-                                            userId = user.userId,
-                                            medium = signUpResult.codeDeliveryDetails.deliveryMedium,
-                                            destination = signUpResult.codeDeliveryDetails.destination
-                                        )
-                                        navController.navigate(UrgentScreen.Confirm.name) {
-
-                                        }
-                                    }
-                                    else {  // back to login
-                                        viewModels.loginViewModel.reset()
-                                        navController.popBackStack()
-                                    }
-                                },
-                                onSignUpFailure = { cause ->
-                                    signUpViewModel.onSignUpFailure(CognitoAuthenticator.reduceFailCause(cause))
-                                },
-                                defaultFailCause = defaultFailCause
+                        onSignUpDone = { cognitoUser, signUpResult ->
+                            viewModels.confirmViewModel.updateConfirmationIdentity(
+                                userId = cognitoUser.userId,
+                                medium = signUpResult.codeDeliveryDetails.deliveryMedium,
+                                destination = signUpResult.codeDeliveryDetails.destination
                             )
+                            viewModels.confirmViewModel.reset()
+                            navController.navigate(UrgentScreen.Confirm.name)
+                        },
+                        onNavigateBack = {
+                            viewModels.loginViewModel.reset()
+                            navController.popBackStack()
                         }
                     )
                 }
@@ -100,32 +99,27 @@ fun UrgentNavHost(
                 // Confirm Screen
                 composable(
                     route = UrgentScreen.Confirm.name,
-                    enterTransition = { EnterTransition.None }
+                    enterTransition = {
+                        slideIntoContainer(
+                            towards = AnimatedContentScope.SlideDirection.Up,
+                            animationSpec = tween(durationMillis = NavConfig.ENTER_TRANSITION_DURATION)
+                        )
+                    },
+                    exitTransition = {
+                        slideOutOfContainer(
+                            towards = AnimatedContentScope.SlideDirection.Down,
+                            animationSpec = tween(durationMillis = NavConfig.EXIT_TRANSITION_DURATION + 200)
+                        )
+                    }
                 ) {
-                    val defaultFailCause = stringResource(id = R.string.ui_confirmScreen_defaultFailCause)
                     val confirmViewModel = viewModels.confirmViewModel
                     ConfirmScreen(
                         confirmViewModel = confirmViewModel,
-                        confirmMedium = confirmViewModel.medium,
-                        confirmDestination = confirmViewModel.destination,
-                        onConfirmRequest = {
-                            // This will cause confirmation to fail if the user attribute
-                            // has been verified for another user in the same pool
-                            val forcedAliasCreation = false
-                            cognito.confirmSignUp(
-                                userId = confirmViewModel.userId,
-                                confirmationCode = confirmViewModel.confirmCodeTextInput,
-                                forcedAliasCreation = forcedAliasCreation,
-                                onConfirmSuccess = confirmViewModel::onConfirmSuccess,
-                                onConfirmFailure = { cause ->
-                                    confirmViewModel.onConfirmFailure(CognitoAuthenticator.reduceFailCause(cause))
-                                },
-                                defaultFailCause = defaultFailCause
-                            )
-                        },
-                        onBackToLogInPressed = {
+                        onBackToLogIn = {
                             viewModels.loginViewModel.reset()
-                            navController.navigate(UrgentScreen.Login.name)
+                            navController.navigate(UrgentScreen.Login.name) {
+                                popUpTo(UrgentScreen.Login.name) { inclusive = true }
+                            }
                         }
                     )
                 }
@@ -134,70 +128,32 @@ fun UrgentNavHost(
                 composable(
                     route = UrgentScreen.Login.name,
                     enterTransition = {
-                        fadeIn(animationSpec = tween(durationMillis = 700))
+                        fadeIn(animationSpec = tween(durationMillis = NavConfig.ENTER_TRANSITION_DURATION))
                     },
                     exitTransition = {
-                        fadeOut(animationSpec = tween(durationMillis = 500))
+                        fadeOut(animationSpec = tween(durationMillis = NavConfig.EXIT_TRANSITION_DURATION))
                     },
                 ) {
-                    val defaultFailCause = stringResource(id = R.string.ui_loginScreen_defaultFailCause)
                     val loginViewModel = viewModels.loginViewModel
                     LoginScreen(
                         loginViewModel = loginViewModel,
-                        onLoginRequest = {
-                            cognito.loginUser(
-                                userId = loginViewModel.emailTextInput,
-                                onLoginSuccess = {
-                                    navController.navigate(UrgentScreen.Home.name) {
-                                        popUpTo(UrgentScreen.Login.name) { inclusive = true }
-                                    }
-                                },
-                                onLoginFailure = { cause ->
-                                    loginViewModel.onLoginFailure(CognitoAuthenticator.reduceFailCause(cause))
-                                },
-                                onAuthenticationDetailsRequest = { continuation, _ ->
-                                    if (continuation == null) {
-                                        loginViewModel.onLoginFailure(defaultFailCause)
-                                    }
-                                    else {
-                                        val authenticationDetails = AuthenticationDetails(
-                                            loginViewModel.emailTextInput,
-                                            loginViewModel.passwordTextInput,
-                                            null,
-                                        )
-                                        continuation.setAuthenticationDetails(authenticationDetails)
-                                        continuation.continueTask()
-                                    }
-                                },
-                                onMFACodeRequest = {
-
-                                },
-                                onAuthChallengeRequest = {
-
-                                },
-                                defaultFailCause = defaultFailCause
-                            )
+                        onLoginDone = { userSession -> // TODO
+                            navController.navigate(UrgentScreen.Home.name) {
+                                popUpTo(UrgentScreen.Login.name) { inclusive = true }
+                            }
                         },
                         onNavigateToSignUp = {
                             viewModels.signUpViewModel.reset()
                             navController.navigate(UrgentScreen.SignUp.name)
                         },
-                        onReconfirmRequest = {
-                            cognito.resendSignUpConfirmation(
-                                userId = loginViewModel.emailTextInput,
-                                onResendConfirmationSuccess = { verificationCodeDeliveryMedium ->
-                                    viewModels.confirmViewModel.reset()
-                                    viewModels.confirmViewModel.updateConfirmationIdentity(
-                                        userId = loginViewModel.emailTextInput,
-                                        medium = verificationCodeDeliveryMedium.deliveryMedium,
-                                        destination = verificationCodeDeliveryMedium.destination
-                                    )
-                                    navController.navigate(UrgentScreen.Confirm.name)
-                                },
-                                onResendConfirmationFailure = { cause ->
-                                    loginViewModel.onLoginFailure(CognitoAuthenticator.reduceFailCause(cause))
-                                },
+                        onResendSignUpConfirmationDone = { userId, codeDeliveryDetails ->
+                            viewModels.confirmViewModel.reset()
+                            viewModels.confirmViewModel.updateConfirmationIdentity(
+                                userId = userId,
+                                medium = codeDeliveryDetails.deliveryMedium,
+                                destination = codeDeliveryDetails.destination
                             )
+                            navController.navigate(UrgentScreen.Confirm.name)
                         }
                     )
                 }
@@ -207,7 +163,10 @@ fun UrgentNavHost(
                     route = UrgentScreen.Home.name,
                     enterTransition = { EnterTransition.None }
                 ) {
-                    HomeScreen()
+                    val homeViewModel = viewModels.homeViewModel
+                    HomeScreen(
+                        homeViewModel = homeViewModel,
+                    )
                 }
             }
         }
