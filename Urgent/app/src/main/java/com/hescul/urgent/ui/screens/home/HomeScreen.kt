34c028917b24e5ae14531522a3acb54ef8ad4b3d
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:45:28 +0700

    set up UI for home screen

--- Urgent/app/src/main/java/com/hescul/urgent/ui/screens/home/HomeScreen.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/screens/home/HomeScreen.kt
@@ -1,36 +1,92 @@
 package com.hescul.urgent.ui.screens.home
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hescul.urgent.ui.theme.UrgentTheme
 import com.hescul.urgent.ui.versatile.UrgentTopBar
+import com.hescul.urgent.R
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier
+    homeViewModel: HomeViewModel,
+    modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
+    val contentPadding = 12.dp
+    val patientListState = rememberLazyListState()
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            UrgentTopBar(
+                title = stringResource(id = R.string.ui_homeScreen_title),
+                onLeftActionClick = { /*TODO*/ },
+                onRightActionClick = { /*TODO*/ },
+            )
+        },
+        floatingActionButton = {
+            HomeFAB(
+                onCLick = homeViewModel::onAddPatient,
+                extended = patientListState.firstVisibleItemScrollOffset == 0
+            )
+        }
     ) {
-        UrgentTopBar(
-            title = "Home",
-            onNavigateBack = { /*TODO*/ },
-            onLeftActionClick = { /*TODO*/ },
-            onRightActionClick = { /*TODO*/ }
-        )
+        LazyColumn(
+            state = patientListState,
+            contentPadding = PaddingValues(
+                horizontal = contentPadding * 2,
+                vertical = contentPadding
+            ),
+        ) {
+            item { 
+                Text(
+                    text = stringResource(id = R.string.ui_homeScreen_contentTitle),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(vertical = contentPadding / 2)
+                )
+            }
+            items(
+                items = homeViewModel.patients,
+                key = {
+                    it.deviceId
+                }
+            ) { patient ->
+                PatientChip(
+                    patient = patient,
+                    onCLick = { /*TODO*/ },
+                    chipPadding = contentPadding,
+                    modifier = Modifier.animateItemPlacement(
+                        animationSpec = tween(400)
+                    )
+                )
+            }
+        }
     }
 }
 
 @Preview("Home Screen")
 @Composable
 fun PreviewHomeScreen() {
+    val homeViewModel = HomeViewModel()
     UrgentTheme {
         Surface {
-            HomeScreen()
+            HomeScreen(
+                homeViewModel = homeViewModel,
+            )
         }
     }
 }
