ad5d46c0354004c5a5c741d8535e1703d5493e99
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:48:13 +0700

    migrate to singleton usage for the `CognitoAuthenticator` class

--- Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
@@ -1,59 +1,43 @@
 package com.hescul.urgent
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.amazonaws.ClientConfiguration
-import com.hescul.urgent.core.auth.cognito.CognitoAuthenticator
-import com.hescul.urgent.core.auth.cognito.CognitoConfig
-import com.hescul.urgent.core.auth.cognito.CognitoIdentity
 import com.hescul.urgent.navigation.UrgentNavHost
 import com.hescul.urgent.navigation.UrgentScreen
 import com.hescul.urgent.ui.screens.confirm.ConfirmViewModel
+import com.hescul.urgent.ui.screens.home.HomeViewModel
 import com.hescul.urgent.ui.screens.login.LoginViewModel
+import com.hescul.urgent.ui.screens.opening.OpeningViewModel
 import com.hescul.urgent.ui.screens.signup.SignUpViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val vms = initViewModels()
-        val cognito = initCognito(applicationContext)
+        val viewModels = initViewModels()
         setContent {
             UrgentNavHost(
-                startDestination = UrgentScreen.Login.name,
-                cognito = cognito,
-                viewModels = vms,
+                startDestination = UrgentScreen.Opening.name,
+                viewModels = viewModels,
             )
         }
 
     }
 
     private fun initViewModels(): UrgentViewModels {
+        val openingViewModel by viewModels<OpeningViewModel>()
         val signUpViewModel by viewModels<SignUpViewModel>()
         val confirmViewModel by viewModels<ConfirmViewModel>()
         val loginViewModel by viewModels<LoginViewModel>()
+        val homeViewModel by viewModels<HomeViewModel>()
         return UrgentViewModels(
+            openingViewModel = openingViewModel,
             loginViewModel = loginViewModel,
             signUpViewModel = signUpViewModel,
-            confirmViewModel = confirmViewModel
+            confirmViewModel = confirmViewModel,
+            homeViewModel = homeViewModel
         )
-    }
-
-    private fun initCognito(context: Context): CognitoAuthenticator {
-        val cognitoIdentity = CognitoIdentity(
-            poolId = CognitoConfig.POOL_ID,
-            clientId = CognitoConfig.CLIENT_ID,
-            clientSecret = CognitoConfig.CLIENT_SECRET,
-            region = CognitoConfig.REGION
-        )
-        val cognitoConfig = ClientConfiguration()
-        cognitoConfig.connectionTimeout = CognitoConfig.CONNECTION_TIMEOUT
-        cognitoConfig.socketTimeout = CognitoConfig.SOCKET_TIMEOUT
-        cognitoConfig.maxConnections = CognitoConfig.MAX_CONNECTIONS
-        cognitoConfig.userAgent = CognitoConfig.USER_AGENT
-        return CognitoAuthenticator(context, cognitoIdentity, cognitoConfig)
     }
 }
 
