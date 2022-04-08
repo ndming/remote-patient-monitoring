6b9092c4a7bdd8287a11e7fe09deca3c5b5c0de5
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-08 19:39:07 +0700

    add status color set and healthcare color set

--- Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
@@ -13,6 +13,15 @@ val Teal200 = Color(0xFF03DAC5)
 val Red700 = Color(0xFFD50000)
 val Green800 = Color(0xFF2E7D32)
 
+// status
+val OnlineColor = Color(0xFF98CBF7)
+val OfflineColor = Color(0xFFC3C7C7)
+val ErrorColor = Color(0xFFFCB6B6)
+
 // light theme
 val BluePrimary = Color(0xFF1F5BD6)
 val BlueSecondary = Color(0xFFDCEEFD)
+
+// healthcare
+val PulseIconColor = Color(0xFFF75E5E)
+val Spo2IconColor = Color(0xFF56ADAD)
