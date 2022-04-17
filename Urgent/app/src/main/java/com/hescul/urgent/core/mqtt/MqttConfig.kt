219863c74aeadbc54755b7db45af4453f3b0940c
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:09:28 +0700

    add symbolic constants for `MqttClientConfig`

--- Urgent/app/src/main/java/com/hescul/urgent/core/mqtt/MqttConfig.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/mqtt/MqttConfig.kt
@@ -1,8 +1,8 @@
 package com.hescul.urgent.core.mqtt
 
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos
-import com.amazonaws.regions.Regions
 
+@Suppress("SpellCheckingInspection")
 object MqttBrokerConfig {
     const val END_POINT = "a2r0f2fq44oocy-ats.iot.us-east-1.amazonaws.com"
     const val DOCTOR_POLICY = "RPMDOC_policy"
@@ -10,10 +10,17 @@ object MqttBrokerConfig {
 }
 
 object MqttClientConfig {
-    const val DATA_TOPIC_PREFIX = "rpm/sos/"
-    const val STATUS_TOPIC_PREFIX = "rpm/tus/"
+    const val DATA_TOPIC_KEY = "sos"
+    const val STATUS_TOPIC_KEY = "tus"
+    const val DOCTOR_TOPIC_KEY = "doc"
+    const val TOPIC_SEPARATOR = '/'
+    const val DATA_TOPIC_PREFIX = "rpm$TOPIC_SEPARATOR$DATA_TOPIC_KEY$TOPIC_SEPARATOR"
+    const val STATUS_TOPIC_PREFIX = "rpm$TOPIC_SEPARATOR$STATUS_TOPIC_KEY$TOPIC_SEPARATOR"
+    const val DOCTOR_TOPIC_PREFIX = "rpm$TOPIC_SEPARATOR$DOCTOR_TOPIC_KEY$TOPIC_SEPARATOR"
+
     val SUBSCRIBE_QOS = AWSIotMqttQos.QOS1
 
     const val MAX_AUTO_RECONNECTION_ATTEMPTS = 2
-    const val CLEAN_SESSION = false
+    const val CLEAN_SESSION = true
+    const val AUTO_RESUBSCRIBE = false
 }
