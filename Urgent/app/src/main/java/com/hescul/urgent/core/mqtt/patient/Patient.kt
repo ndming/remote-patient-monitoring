0ef39c8b0ea7703612cc7938f3ba4e7bfc0ccb17
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:10:45 +0700

    modify property declaration for `Patient` class

--- Urgent/app/src/main/java/com/hescul/urgent/core/mqtt/patient/Patient.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/core/mqtt/patient/Patient.kt
@@ -19,17 +19,16 @@ import java.util.*
 
 class Patient(
     val deviceId: String,
-    val name: String,
-    val attributes: MutableList<PatientAttribute> = mutableListOf(),
-    var picture: ImageVector = DEFAULT_PATIENT_PICTURE,
+    val name: String
 ) {
     var status by mutableStateOf(DEFAULT_PATIENT_STATUS)
     var time by mutableStateOf(DEFAULT_UNKNOWN_VALUE)
         private set
     val data = listOf(
-        PatientData(index = PatientIndex.Pulse),
-        PatientData(index = PatientIndex.Spo2)
+        PatientData(index = PatientIndex.Pulse, initialValue = DEFAULT_UNKNOWN_VALUE),
+        PatientData(index = PatientIndex.Spo2, initialValue = DEFAULT_UNKNOWN_VALUE)
     )
+    val attributes: MutableList<PatientAttribute> = mutableListOf()
 
     fun updateTimestamp(epoch: Long, locale: Locale) {
         time = TIME_FORMATTER.withLocale(locale).format(Instant.ofEpochSecond(epoch))
@@ -47,24 +46,18 @@ class Patient(
         @Suppress("SpellCheckingInspection")
         val SAMPLE_PATIENT = Patient(
             deviceId = "RPMSOS0000",
-            name = "Bling Bling",
-            attributes = mutableListOf(
-                PatientAttribute(key = "gender", value = "female", isPinned = true),
-                PatientAttribute(key = "urgent level", value = "hot", isPinned = false),
-                PatientAttribute(key = "age", value = "20", isPinned = true),
-                PatientAttribute(key = "discharged date", value = "14/08", isPinned = false),
-                PatientAttribute(key = "appointment", value = "20/08", isPinned = false),
-                PatientAttribute(key = "illness", value = "cute", isPinned = true)
-            )
+            name = "Bling Bling"
         )
     }
 }
 
-data class PatientAttribute(
-    var key: String,
-    var value: String,
-    var isPinned: Boolean = false
-)
+class PatientAttribute(key: String, value: String, pinned: Boolean = false) {
+    var key by mutableStateOf(key)
+    var value by mutableStateOf(value)
+    var pinned by mutableStateOf(pinned)
+
+    fun clone() = PatientAttribute(key, value, pinned)
+}
 
 enum class PatientStatus(val code: Int, val tint: Color) {
     Online(code = 0, tint = OnlineColor),
@@ -72,7 +65,7 @@ enum class PatientStatus(val code: Int, val tint: Color) {
 }
 
 enum class PatientIndex(
-    @StringRes val title: Int,
+    @StringRes val label: Int,
     @StringRes val unit: Int,
     val icon: ImageVector,
     val tint: Color,
@@ -80,7 +73,7 @@ enum class PatientIndex(
     val key: String,
 ) {
     Pulse(
-        title = R.string.ui_profileScreen_pulseIndexName,
+        label = R.string.ui_profileScreen_pulseIndexName,
         unit = R.string.ui_profileScreen_pulseIndexUnit,
         icon = Icons.Default.Favorite,
         tint = PulseIconColor,
@@ -88,7 +81,7 @@ enum class PatientIndex(
         key = "pulse"
     ),
     Spo2(
-        title = R.string.ui_profileScreen_spo2IndexName,
+        label = R.string.ui_profileScreen_spo2IndexName,
         unit = R.string.ui_profileScreen_spo2IndexUnit,
         icon = Icons.Default.Bloodtype,
         tint = Spo2IconColor,
@@ -97,7 +90,7 @@ enum class PatientIndex(
     )
 }
 
-class PatientData(val index: PatientIndex) {
-    var value by mutableStateOf(Patient.DEFAULT_UNKNOWN_VALUE)
+class PatientData(val index: PatientIndex, initialValue: String) {
+    var value by mutableStateOf(initialValue)
 }
 
