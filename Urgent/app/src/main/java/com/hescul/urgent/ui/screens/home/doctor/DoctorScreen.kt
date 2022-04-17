1750d9ae95b1bc306ce7fa9a7cf201e0f9a163b1
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:15:34 +0700

    a doctor screen for `HomeScreen`

---
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/screens/home/doctor/DoctorScreen.kt
@@ -0,0 +1,17 @@
+package com.hescul.urgent.ui.screens.home.doctor
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DoctorScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+    }
+}
