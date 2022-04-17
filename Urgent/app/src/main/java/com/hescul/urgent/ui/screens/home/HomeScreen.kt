93c5039f56fbf17b4172ce6e7dd82b6a364bf5ba
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:08:00 +0700

    decompose home screen into multiple composable destination

--- Urgent/app/src/main/java/com/hescul/urgent/ui/screens/home/HomeScreen.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/screens/home/HomeScreen.kt
@@ -1,78 +1,130 @@
 package com.hescul.urgent.ui.screens.home
 
+import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.hescul.urgent.ui.theme.UrgentTheme
 import com.hescul.urgent.ui.versatile.UrgentTopBar
 import com.hescul.urgent.R
+import com.hescul.urgent.navigation.HomeScreens
+import com.hescul.urgent.ui.screens.home.doctor.DoctorScreen
+import com.hescul.urgent.ui.screens.home.patient.*
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     homeViewModel: HomeViewModel,
+    patientViewModel: PatientViewModel,
     modifier: Modifier = Modifier,
 ) {
-    val contentPadding = 12.dp
+    LaunchedEffect(Unit) {
+        patientViewModel.onLaunch()
+    }
     val patientListState = rememberLazyListState()
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
-            UrgentTopBar(
-                title = stringResource(id = R.string.ui_homeScreen_title),
-                onLeftActionClick = { /*TODO*/ },
-                onRightActionClick = { /*TODO*/ },
-            )
-        },
-        floatingActionButton = {
-            HomeFAB(
-                onCLick = homeViewModel::onAddPatient,
-                extended = patientListState.firstVisibleItemScrollOffset == 0
+    val scaffoldState = rememberScaffoldState()
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val coroutineScope = rememberCoroutineScope()
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp, bottomStart = 0.dp, bottomEnd = 0.dp),
+        sheetBackgroundColor = MaterialTheme.colors.surface,
+        sheetContent = {
+            SubscribeSheet(
+                patientViewModel = patientViewModel,
+                onSubscribeRequest = {
+                    patientViewModel.onSubscribeRequest(
+                        onDeviceSatisfied = {
+                            coroutineScope.launch {
+                                sheetState.animateTo(
+                                    targetValue = ModalBottomSheetValue.Hidden,
+                                    anim = tween(durationMillis = 800)
+                                )
+                            }
+                        }
+                    )
+                }
             )
         }
     ) {
-        LazyColumn(
-            state = patientListState,
-            contentPadding = PaddingValues(
-                horizontal = contentPadding * 2,
-                vertical = contentPadding
-            ),
-        ) {
-            item { 
-                Text(
-                    text = stringResource(id = R.string.ui_homeScreen_contentTitle),
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier.padding(vertical = contentPadding / 2)
+        val homeNavController = rememberAnimatedNavController()
+        Scaffold(
+            scaffoldState = scaffoldState,
+            modifier = modifier.fillMaxSize(),
+            topBar = {
+                UrgentTopBar(
+                    title = stringResource(id = when(homeViewModel.currentScreen) {
+                        HomeScreens.Doctor.route -> R.string.ui_homeScreen_doctorScreenTitle
+                        else -> R.string.ui_homeScreen_patientScreenTitle
+                    }),
+                    showNavigateBack = false,
+                    showRightAction = false
+                )
+            },
+            floatingActionButton = {
+                HomeFAB(
+                    onCLick = {
+                        coroutineScope.launch {
+                            sheetState.animateTo(
+                                targetValue = ModalBottomSheetValue.Expanded,
+                                anim = tween(durationMillis = 800)
+                            )
+                        }
+                    },
+                    extended = patientListState.firstVisibleItemScrollOffset == 0,
+                    enabled = !patientViewModel.isProgressing
+                )
+            },
+            bottomBar = {
+                HomeBottomBar(
+                    homeNavController = homeNavController,
+                    screens = listOf(HomeScreens.Patient, HomeScreens.Doctor),
+                    onScreenChange = homeViewModel::onCurrentScreenChange,
+                    enabled = !patientViewModel.isProgressing
                 )
             }
-            items(
-                items = homeViewModel.patients,
-                key = {
-                    it.deviceId
-                }
-            ) { patient ->
-                PatientChip(
-                    patient = patient,
-                    onCLick = { /*TODO*/ },
-                    chipPadding = contentPadding,
-                    modifier = Modifier.animateItemPlacement(
-                        animationSpec = tween(400)
-                    )
-                )
+        ) { contentPadding ->
+            AnimatedNavHost(
+                navController = homeNavController,
+                startDestination = HomeScreens.Patient.route,
+                modifier = Modifier.padding(contentPadding)
+            ) {
+                composable(
+                    HomeScreens.Patient.route,
+                    enterTransition = {
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Right,
+                            animationSpec = tween(durationMillis = 600)
+                        )
+                    },
+                    exitTransition = {
+                        fadeOut(animationSpec = tween(durationMillis = 400))
+                    }
+                ) { PatientScreen(patientViewModel, patientListState) }
+                composable(
+                    HomeScreens.Doctor.route,
+                    enterTransition = {
+                        slideIntoContainer(
+                            AnimatedContentScope.SlideDirection.Left,
+                            animationSpec = tween(durationMillis = 600)
+                        )
+                    },
+                    exitTransition = {
+                        fadeOut(animationSpec = tween(durationMillis = 400))
+                    }
+                ) { DoctorScreen() }
             }
         }
     }
@@ -82,10 +134,12 @@ fun HomeScreen(
 @Composable
 fun PreviewHomeScreen() {
     val homeViewModel = HomeViewModel()
+    val patientViewModel = PatientViewModel(LocalContext.current)
     UrgentTheme {
         Surface {
             HomeScreen(
                 homeViewModel = homeViewModel,
+                patientViewModel = patientViewModel,
             )
         }
     }
