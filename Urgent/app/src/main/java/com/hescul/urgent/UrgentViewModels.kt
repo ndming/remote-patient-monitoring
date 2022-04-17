7ec80d33bc1a87a902da6a6a2e8e280ccb0fb13e
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:13:02 +0700

    add `PatientViewModel` attribute

--- Urgent/app/src/main/java/com/hescul/urgent/UrgentViewModels.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/UrgentViewModels.kt
@@ -2,6 +2,7 @@ package com.hescul.urgent
 
 import com.hescul.urgent.ui.screens.confirm.ConfirmViewModel
 import com.hescul.urgent.ui.screens.home.HomeViewModel
+import com.hescul.urgent.ui.screens.home.patient.PatientViewModel
 import com.hescul.urgent.ui.screens.login.LoginViewModel
 import com.hescul.urgent.ui.screens.opening.OpeningViewModel
 import com.hescul.urgent.ui.screens.signup.SignUpViewModel
@@ -12,4 +13,5 @@ data class UrgentViewModels(
     val signUpViewModel: SignUpViewModel,
     val confirmViewModel: ConfirmViewModel,
     val homeViewModel: HomeViewModel,
+    val patientViewModel: PatientViewModel,
 )
