7d9d05163456beda5bedb0f9a803231903a7a808
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-17 23:03:47 +0700

    add dynamic locale preference

--- Urgent/app/src/main/java/com/hescul/urgent/UrgentApplication.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/UrgentApplication.kt
@@ -1,7 +1,9 @@
 package com.hescul.urgent
 
 import android.app.Application
+import android.content.res.Configuration
 import timber.log.Timber
+import java.util.*
 
 class UrgentApplication : Application() {
     override fun onCreate() {
@@ -9,5 +11,16 @@ class UrgentApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        systemLocale = Locale.getDefault()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        systemLocale = newConfig.locales[0]
+    }
+
+    companion object {
+        lateinit var systemLocale: Locale
+            private set
     }
 }
