3d3949dd4197d07dc03e5cd32dcf6cfa2ca7b384
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-20 15:29:10 +0700

    add secondary variant

--- Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Theme.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
@@ -19,7 +20,7 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = BlueSecondary,
     error = Red700,
-
+    secondaryVariant = Color.Black.copy(0.05f)
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
