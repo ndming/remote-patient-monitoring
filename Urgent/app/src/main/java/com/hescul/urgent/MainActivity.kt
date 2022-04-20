820819c223fd135aaf47009582bd7e0c70ff4a1a
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-20 15:25:22 +0700

    update MainActivity.kt

--- Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/MainActivity.kt
@@ -9,10 +9,12 @@ import com.hescul.urgent.navigation.UrgentNavHost
 import com.hescul.urgent.navigation.UrgentScreens
 import com.hescul.urgent.ui.screens.confirm.ConfirmViewModel
 import com.hescul.urgent.ui.screens.home.HomeViewModel
+import com.hescul.urgent.ui.screens.home.doctor.DoctorViewModel
 import com.hescul.urgent.ui.screens.home.patient.PatientViewModel
 import com.hescul.urgent.ui.screens.home.patient.PatientViewModelFactory
 import com.hescul.urgent.ui.screens.login.LoginViewModel
 import com.hescul.urgent.ui.screens.opening.OpeningViewModel
+import com.hescul.urgent.ui.screens.profile.ProfileViewModel
 import com.hescul.urgent.ui.screens.signup.SignUpViewModel
 
 class MainActivity : ComponentActivity() {
@@ -35,13 +37,17 @@ class MainActivity : ComponentActivity() {
         val loginViewModel by viewModels<LoginViewModel>()
         val homeViewModel by viewModels<HomeViewModel>()
         val patientViewModel by viewModels<PatientViewModel>(factoryProducer = { PatientViewModelFactory(context) })
+        val doctorViewModel by viewModels<DoctorViewModel>()
+        val profileViewModel by viewModels<ProfileViewModel>()
         return UrgentViewModels(
             openingViewModel = openingViewModel,
             loginViewModel = loginViewModel,
             signUpViewModel = signUpViewModel,
             confirmViewModel = confirmViewModel,
             homeViewModel = homeViewModel,
-            patientViewModel = patientViewModel
+            patientViewModel = patientViewModel,
+            doctorViewModel = doctorViewModel,
+            profileViewModel = profileViewModel
         )
     }
 }
