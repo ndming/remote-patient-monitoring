170576df411040d7c3c19d6cff62453dafd5ef34
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-25 21:45:51 +0700

    increase transition time for navigation

--- Urgent/app/src/main/java/com/hescul/urgent/navigation/NavConfig.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/navigation/NavConfig.kt
@@ -1,6 +1,6 @@
 package com.hescul.urgent.navigation
 
 object NavConfig {
-    const val ENTER_TRANSITION_DURATION = 400
-    const val EXIT_TRANSITION_DURATION = 200
+    const val ENTER_TRANSITION_DURATION = 550
+    const val EXIT_TRANSITION_DURATION = 350
 }
