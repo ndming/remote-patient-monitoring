852a88259e382fc279c39c68d97f8eb6b49f3e05
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-25 21:39:50 +0700

    remove default color

--- Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/theme/Color.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 // defaults
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
