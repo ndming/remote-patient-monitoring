580a05e8b889047c5ae3c3518d3cb3dc428626d4
hescul <minh.nguyen207bk@hcmut.edu.vn>
2022-04-25 21:46:33 +0700

    add auto login functionality

--- Urgent/app/src/main/java/com/hescul/urgent/ui/screens/opening/OpeningViewModel.kt
+++ Urgent/app/src/main/java/com/hescul/urgent/ui/screens/opening/OpeningViewModel.kt
@@ -1,19 +1,29 @@
 package com.hescul.urgent.ui.screens.opening
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+
+import android.content.Context
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.delay
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession
+import com.hescul.urgent.core.cognito.CognitoAuthenticator
 
 class OpeningViewModel : ViewModel() {
-    companion object {
-        private const val OPENING_DURATION = 1000L  // in ms
-    }
 
-    suspend fun showOpening(onDone: () -> Unit) {
-        delay(OPENING_DURATION)
-        onDone()
+    fun onLaunch(context: Context, onAutoLoginSuccess: (CognitoUserSession) -> Unit, onAutoLoginFailure: (String) -> Unit) {
+        val userId = CognitoAuthenticator.getCurrentUserId(context)
+        if (userId == null) {
+            onAutoLoginFailure("")
+        }
+        else {
+            CognitoAuthenticator.requestLogIn(
+                context = context,
+                userId = userId,
+                onLoginSuccess = onAutoLoginSuccess,
+                onLoginFailure = { onAutoLoginFailure(userId) },
+                onAuthenticationDetailsRequest = { onAutoLoginFailure(userId) },
+                onAuthChallengeRequest = { onAutoLoginFailure(userId) },
+                onMFACodeRequest = { onAutoLoginFailure(userId) }
+            )
+        }
     }
 
 
